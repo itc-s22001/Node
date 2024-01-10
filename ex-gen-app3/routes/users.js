f7433a45bba61243def66469b70a79cce6202cba
s22001 <s22001@std.it-college.ac.jp>
2024-01-10 16:06:27 +0900

    idを指定して１件だけの表示できた

--- ex-gen-app3/routes/users.js
+++ ex-gen-app3/routes/users.js
@@ -4,12 +4,28 @@ const prisma = new PrismaClient();
 
 /* GET users listing. */
 router.get("/", async (req, res, next) => {
-  const users = await prisma.user.findMany();
+  const id = +req.query.id;　// Number(req.query.id); parseInt(req.query.id); どれかを使う
+  if (!id) {
+    // idが指定されていない場合
+    const users = await prisma.user.findMany();
+    const data = {
+      title: "Users/Index",
+      content: users
+    };
+    res.render("users/index", data);
+    return;
+  }
+  // idが指定されていた場合
+  const user = await prisma.user.findUnique({  // findUniqueは主キーかユニークのみ取れる
+    where: {
+      id: id
+    }
+  });
   const data = {
     title: "Users/Index",
-    content: users
+    content: [user]
   };
-  res.render("users/index", data);
+  res.render("users/index", data)
 });
 
 module.exports = router;
