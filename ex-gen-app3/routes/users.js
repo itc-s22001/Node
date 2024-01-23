06f2b339232a584405973db42be0c24d2df87725
s22001 <s22001@std.it-college.ac.jp>
2024-01-23 15:10:34 +0900

    initial commit for chap7-2

--- ex-gen-app3/routes/users.js
+++ ex-gen-app3/routes/users.js
@@ -2,43 +2,17 @@ const router =  require("express").Router();
 const {PrismaClient} = require("@prisma/client");
 const prisma = new PrismaClient();
 
+const pageSize = 3; //ユーザー一覧での１画面あたりに表示するレコード数
+
 /* GET users listing. */
 router.get("/", async (req, res, next) => {
-  const id = +req.query.id;　// Number(req.query.id); parseInt(req.query.id); どれかを使う
-  if (id) {
-    // idが指定されていた場合
-    const user = await prisma.user.findUnique({  // findUniqueは主キーかユニークのみ取れる
-      where: {
-        id: id
-      }
-    });
-    const data = {
-      title: "Users/Index",
-      content: [user]
-    };
-    res.render("users/index", data)
-    return;
-  }
-
-  const age = +req.query.age;
-  if (age) {
-    // 年齢(age)が条件として指定された場合
-    const users = await prisma.user.findMany({
-      where: {
-        age: {
-          lte: age
-        }
-      }
-    });
-    const data = {
-      title: "Users/Index",
-      content: users
-    };
-    res.render("users/index", data);
-    return;
-  }
   // 無条件で全データを表示したい場合
-  const users = await prisma.user.findMany();
+  const page = req.query.page ? +req.query.page : 0;
+  const users = await prisma.user.findMany({
+    orderBy: [{name: "asc"}],
+    skip: pageSize * (page - 1),
+    take: pageSize
+  });
   const data = {
     title: "Users/Index",
     content: users
